a995edff39255ecf812442b0680841d8abc9f5ab
Avery Magnotti <averymagnotti@outlook.com>
2016-11-02 19:58:29 -0400

    Ignore png assets

--- setup.sh
+++ setup.sh
@@ -5,16 +5,17 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 rsync --exclude ".git/" \
-  --exclude "COLORS.md" \
-  --exclude "README.md" \
-  --exclude "LICENSE.md" \
-  --exclude "setup.sh" \
-  -avh --no-perms . ~;
-  source .bashrc;
-  if [ "$(uname)" == "Darwin" ]; then
-    ./.macos
-  elif [ "$(echo $TERM_PROGRAM)" == "iTerm.app" ]; then
-    open dotfiles.itermcolors
-  elif [ "$(lsb_release -si)" == "elementary" ]; then
-    ./.pantheon
-  fi
+--exclude "*.png" \
+--exclude "COLORS.md" \
+--exclude "README.md" \
+--exclude "LICENSE.md" \
+--exclude "setup.sh" \
+-avh --no-perms . ~;
+source .bashrc;
+if [ "$(uname)" == "Darwin" ]; then
+  ./.macos
+elif [ "$(echo $TERM_PROGRAM)" == "iTerm.app" ]; then
+  open dotfiles.itermcolors
+elif [ "$(lsb_release -si)" == "elementary" ]; then
+  ./.pantheon
+fi
