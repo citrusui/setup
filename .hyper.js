1522cd92f1a49247bb737d58a8fa2db15539e8f1
Avery Magnotti <averymagnotti@outlook.com>
2016-12-14 15:22:37 -0500

    Remove Hot Corner in top left as it is annoying

--- .hyper.js
+++ .hyper.js
@@ -1,90 +1,41 @@
+// More info: https://hyper.is/#cfg
+
 module.exports = {
   config: {
-    // default font size in pixels for all tabs
     fontSize: 13,
-
-    // font family with optional fallbacks
-    fontFamily: 'Menlo, "Roboto Mono", "Lucida Console", monospace',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: '#6b7a86',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
-
-    // color of the text
-    foregroundColor: '#f9f9f9',
-
-    // terminal background color
-    backgroundColor: '#292e33',
-
-    // border color (window, tabs)
-    borderColor: '#292e33',
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom css to embed in the terminal window
-    termCSS: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
+    fontFamily: "Menlo, \"Roboto Mono\", \"Lucida Console\", monospace",
+    cursorColor: "#6b7a86",
+    cursorShape: "BLOCK",
+    foregroundColor: "#f9f9f9",
+    backgroundColor: "#292e33",
+    borderColor: "#292e33",
+    css: "",
+    termCSS: "",
+    padding: "12px 14px",
     colors: {
-      black: '#40484f',
-      red: '#e03431',
-      green: '#79ce80',
-      yellow: '#e8e366',
-      blue: '#4ea5d9',
-      magenta: '#e031a8',
-      cyan: '#4ed4d8',
-      white: '#f9f9f9',
-      lightBlack: '#40484f',
-      lightRed: '#e03431',
-      lightGreen: '#79ce80',
-      lightYellow: '#e8e366',
-      lightBlue: '#4ea5d9',
-      lightMagenta: '#e031a8',
-      lightCyan: '#4ed4d8',
-      lightWhite: '#f9f9f9'
+      black: "#40484f",
+      red: "#e03431",
+      green: "#79ce80",
+      yellow: "#e8e366",
+      blue: "#4ea5d9",
+      magenta: "#e031a8",
+      cyan: "#4ed4d8",
+      white: "#f9f9f9",
+      lightBlack: "#40484f",
+      lightRed: "#e03431",
+      lightGreen: "#79ce80",
+      lightYellow: "#e8e366",
+      lightBlue: "#4ea5d9",
+      lightMagenta: "#e031a8",
+      lightCyan: "#4ed4d8",
+      lightWhite: "#f9f9f9"
     },
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    shell: 'bash',
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
+    shell: "bash",
+    shellArgs: ["--login"],
     env: {},
-
-    // set to false for no bell
-    bell: 'SOUND',
-
-    // if true, selected text will automatically be copied to the clipboard
+    bell: "SOUND",
     copyOnSelect: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
   },
-
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: [],
-
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
   localPlugins: []
 };
